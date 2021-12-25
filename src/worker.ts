a335480c6de6c0781fe1c36188e23a2c3df47f6f
Mathspy <mathspy257@gmail.com>
2021-12-25 18:51:17 +0200

    Add a scheduled handler that requests a redeploy

--- src/worker.ts
+++ src/worker.ts
@@ -7,4 +7,28 @@ type Env = {
   AUTH_KEY: string;
 };
 
-export default {} as ExportedHandler<Env>;
+export default {
+  scheduled(_event, env, ctx) {
+    ctx.waitUntil(deploy(env));
+  },
+} as ExportedHandler<Env>;
+
+async function deploy(env: Env) {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${env.ACCOUNT_ID}/pages/projects/${env.PROJECT_NAME}/deployments`,
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+        "X-Auth-Email": env.EMAIL,
+        "X-Auth-Key": env.AUTH_KEY,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    console.error(
+      `Failed to start new deployment.\nCaused by: ${await response.text()}`,
+    );
+  }
+}
