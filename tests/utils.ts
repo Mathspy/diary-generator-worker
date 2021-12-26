aa1dde7962aca51bc64dcf72c55fb5a08c979c87
Mathspy <mathspy257@gmail.com>
2021-12-26 13:46:05 +0200

    Make withWaitUntil return the value from the function passed to it

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,16 +1,18 @@
+type waitUntilFn = (promise: Promise<unknown>) => void;
+
 // Pass a waitUntil function which collects a punch of promises
 //
 // withWaitUntil's returned promise will only resolve once all
 // passed promises resolve
-export async function withWaitUntil(
-  fn: (w: (p: Promise<unknown>) => void) => void | Promise<void>,
-) {
+export async function withWaitUntil<T>(
+  fn: (w: waitUntilFn) => T | Promise<T>,
+): Promise<T> {
   const promises: Promise<unknown>[] = [];
   const waitUntil = (promise: Promise<unknown>) => {
     promises.push(promise);
   };
 
-  await fn(waitUntil);
+  const response = await fn(waitUntil);
 
-  return Promise.all(promises);
+  return Promise.all(promises).then(() => response);
 }
